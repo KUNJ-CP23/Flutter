a8324202a6b563ba50df6c5abe93b06093e51274
Kunj Ramoliya <kunjramoliya367@gmail.com>
2024-12-11 11:28:30 +0530

    Lecture 4/12

---
+++ lib/lab_5/b1.dart
@@ -0,0 +1,6 @@
+//WAP to find the indices of two numbers in an integer array nums that add up to a given target.
+// You may assume that each input has exactly one solution, and you cannot use the same
+// element twice. The answer can be returned in any order. Example: Input: nums = [2, 7, 11,
+// 15], target = 9, Output: [0, 1]; Input: nums = [3, 2, 4], target = 6, Output: [1, 2]; Input:
+// nums = [3, 3], target = 6, Output: [0, 1].
+
--- lib/lecture_prog/first_27_11.dart
+++ lib/lecture_prog/first_27_11.dart
@@ -3,3 +3,8 @@ void main(){
   int b = a!;
   print("B : $b");
 }
+
+//nichena bey same j che to check if its null or not
+// int c = d! = null ? d:0;
+// int c = d ?? 6
+
