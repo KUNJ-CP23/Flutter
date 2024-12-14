df8be4dc7ccae53c03bf107babe99c4a5500a679
Kunj Ramoliya <kunjramoliya367@gmail.com>
2024-12-15 00:23:07 +0530

    something!something

--- lib/lab_2/b3.dart
+++ lib/lab_2/b3.dart
@@ -1,21 +1,42 @@
 //WAP to find the majority element in a given array, where the majority element is the one that
 // appears more than [n / 2] times. You may assume that the majority element always exists in
-// the array. Example: Input: nums = [3, 2, 3], Output: 3; Input: nums = [2, 2, 1, 1, 1, 2, 2], Output:
-// 2.
+// the array. Example: Input: nums = [3, 2, 3], Output: 3; Input: nums = [2, 2, 1, 1, 1, 2, 2], Output:2.
 
 import 'dart:io';
 
 void main() {
-  List<int> numbers = [];
+  stdout.write("Enter the size of the list: ");
+  int size = int.parse(stdin.readLineSync()!);
 
-  for (int i = 1; i <=5; i++) {
-    stdout.write("Enter n" + i.toString() + " : ");
-    int n = int.parse(stdin.readLineSync()!);
-    numbers.add(n);
+  List<int> nums = [];
+  print("Enter $size integers:");
+
+  for (int i = 0; i < size; i++) {
+    stdout.write("Enter integer ${i + 1}: ");
+    int num = int.parse(stdin.readLineSync()!);
+    nums.add(num);
   }
-  stdout.write(numbers);
 
+  print(nums);
 
+}
+int findMajorityElement(List<int> nums) {
+  Map<int, int> freqMap = {};
+
+  // Populate the frequency map
+  for (int num in nums) {
+    if (freqMap.containsKey(num)) {
+      freqMap[num] = freqMap[num]! + 1;
+    } else {
+      freqMap[num] = 1;
+    }
+
+    // Check if this number is the majority element
+    if (freqMap[num]! > nums.length ~/ 2) {
+      return num;
+    }
+  }
+  throw Exception("No majority element found"); // This line assumes input is valid.
 }
 
 // void main() {
