e91d5f58517f4ca894148ac1a262dc53af4ea26d
Kunj Ramoliya <kunjramoliya367@gmail.com>
2025-01-18 23:58:53 +0530

    controller stateful

--- lib/lab_7/controller_3.dart
+++ lib/lab_7/controller_3.dart
@@ -1,35 +1,85 @@
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+//
+// class Controller3 extends StatefulWidget {
+//   const Controller3({super.key});
+//
+//   @override
+//   State<Controller3> createState() => _LayoutState();
+// }
+//
+// class _LayoutState extends State<Controller3> {
+//   TextEditingController name = TextEditingController();
+//   @override
+//   Widget build(BuildContext context) {
+//
+//
+//     return Scaffold(
+//       body: Column(
+//         children: [
+//           TextField(
+//             controller: name,
+//           ),
+//
+//           ElevatedButton(onPressed: (){
+//             print(name.text);
+//             setState(() {
+//
+//             });
+//           },child: Text("submit")),
+//            Text(name.text)
+//         ],
+//       ),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 class Controller3 extends StatefulWidget {
   const Controller3({super.key});
 
   @override
-  State<Controller3> createState() => _LayoutState();
+  State<Controller3> createState() => _TextFieldExampleState();
 }
 
-class _LayoutState extends State<Controller3> {
-  TextEditingController name = TextEditingController();
+class _TextFieldExampleState extends State<Controller3> {
+  final TextEditingController textController = TextEditingController();
+
+  void printInputValue() {
+    // Print the input value to the terminal
+    print("Input value: ${textController.text}");
+  }
+
   @override
   Widget build(BuildContext context) {
-
-
     return Scaffold(
-      body: Column(
-        children: [
-          TextField(
-            controller: name,
-          ),
-
-          ElevatedButton(onPressed: (){
-            print(name.text);
-            setState(() {
-
-            });
-          },child: Text("submit")),
-           Text(name.text)
-        ],
+      appBar: AppBar(
+        title: const Text("TextField Example"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextField(
+              controller: textController,
+              decoration: InputDecoration(
+                labelText: 'Enter Text',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10),
+                ),
+              ),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: printInputValue,
+              child: const Text("Print to Terminal"),
+            ),
+          ],
+        ),
       ),
     );
   }
 }
+
--- lib/main.dart
+++ lib/main.dart
@@ -58,7 +58,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Controller3_2(),
+      home: Controller3(),
     );
   }
 }
