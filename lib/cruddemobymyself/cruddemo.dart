f2f2140b1566b3d88ac65601f5f9dde5f108dd65
Kunj Ramoliya <kunjramoliya367@gmail.com>
2025-01-20 19:47:23 +0530

    crud by myself

--- lib/cruddemobymyself/cruddemo.dart
+++ lib/cruddemobymyself/cruddemo.dart
@@ -1,5 +1,5 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
 
 class CrudDemoK extends StatefulWidget {
   CrudDemoK({super.key});
@@ -12,32 +12,134 @@ class _CrudDemoKState extends State<CrudDemoK> {
   TextEditingController nameController = TextEditingController();
   List<String> names = ["demo", "demo2"];
 
-  // void addItem(String name){
-  //   setState(() {
-  //     nameController.add(name);
-  //   });
+  //add method
+  void addItem(String name){
+    setState(() {
+      if(name.isNotEmpty){
+        names.add(name);
+        //after adding it must cleared
+        nameController.clear();
+      }
+    });
+  }
+
+  //edit method
+  void editItem(int index){
+      nameController.text = names[index];
+      showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text("Edit Item"),
+          content: TextField(
+            controller: nameController,
+            decoration: InputDecoration(labelText: "Enter new name"),
+          ),
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  names[index] = nameController.text;
+                  nameController.clear();
+                });
+                Navigator.pop(context);
+              },
+              child: Text("Update"),
+            ),
+          ],
+        ),
+      );
+
+  }
+
+  //delete method
+  void deleteItem(int index){
+
+    // setState(() {
+    //   names.removeAt(index);
+    // });
+
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+          title: Text("Confirm Delete"),
+          content: Text("Are you sure you want to delete this item?"),
+          actions: [
+            TextButton(
+                onPressed: () => Navigator.pop(context),
+                child: Text("Cancel"),
+            ),
+            TextButton(
+              onPressed: () {
+                setState(() {
+                  names.removeAt(index);
+                });
+                Navigator.pop(context);
+              },
+              child: Text("Delete"),
+            ),
+          ],
+        )
+    );
+  }
+
+  //search method
+  // void searchItem(int index){
   //
   // }
-  void editItem(){
-
-  }
-  void deleteItem(){
-
-  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.pinkAccent,
-        title: Text("Crud Demo K"),
+        backgroundColor: Colors.pinkAccent.shade100,
+        title: const Text("Crud using ListView Builder"),
       ),
       body: Padding(
           padding: const EdgeInsets.all(15.0),
-        child: Column(),
-      )
+        child: Column(
+          children: [
+            TextFormField(
+              controller: nameController,
+              decoration: InputDecoration(
+                labelText: "Enter Text"
+              ),
+            ),
+            SizedBox(height: 10,),
+            ElevatedButton(
+                onPressed: () => addItem(nameController.text),
+                child: const Text("Add"),
+            ),
+            SizedBox(height: 5),
+            Expanded(
+                child: ListView.builder(
+                    itemCount: names.length,
+                    itemBuilder: (context, index){
+                      return ListTile(
+                        title: Text(names[index]),
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                                icon: Icon(Icons.edit, color: Colors.blueAccent,),
+                                onPressed: () => editItem(index)
+                            ),
+                            IconButton(
+                                icon: Icon(Icons.delete, color: Colors.red,),
+                                onPressed: () => deleteItem(index)
+                            )
+                          ],
+                        ),
+                      );
+                    }
+                )
+            )
+          ],
+        ),
+      ),
     );
   }
-
-
 }
