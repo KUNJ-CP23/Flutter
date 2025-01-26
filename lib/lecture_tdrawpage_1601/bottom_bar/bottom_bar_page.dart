69c4ba29567c41b38c401bdbe063e35025180408
Kunj Ramoliya <kunjramoliya367@gmail.com>
2025-01-26 21:43:33 +0530

    bottom bar done

--- lib/lecture_tdrawpage_1601/bottom_bar/bottom_bar_page.dart
+++ lib/lecture_tdrawpage_1601/bottom_bar/bottom_bar_page.dart
@@ -73,3 +73,78 @@ class _BottomBarPageState extends State<BottomBarPage> {
 }
 
 //khali ek icon aapi to error batave k ek rakhine tame suj kari leso so put multiple
+
+//
+// import 'package:btech_sem_4_2024/design/add_edit/user_entry_form.dart';
+// import 'package:btech_sem_4_2024/design/login/launch_page.dart';
+// import 'package:btech_sem_4_2024/design/login/pre_login_page.dart';
+// import 'package:btech_sem_4_2024/design/page_view/flutter_page_view.dart';
+// import 'package:btech_sem_4_2024/design/tab_bar/drawer_page.dart';
+// import 'package:flutter/material.dart';
+//
+// class BottomBarPage extends StatefulWidget {
+//   BottomBarPage({super.key});
+//
+//   @override
+//   State<BottomBarPage> createState() => _BottomBarPageState();
+// }
+//
+// class _BottomBarPageState extends State<BottomBarPage> {
+//   int selectedBottomIndex = 0;
+//   List<Widget> pages = [
+//     FlutterPageView(),
+//     DrawerPage(),
+//     PreLoginPage(),
+//     LaunchPage(),
+//     UserEntryFormPage(),
+//   ];
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('BottomBar Demo'),
+//       ),
+//       body: pages[selectedBottomIndex],
+//       bottomNavigationBar: BottomNavigationBar(
+//         onTap: (index) {
+//           setState(() {
+//             selectedBottomIndex = index;
+//           });
+//         },
+//         currentIndex: selectedBottomIndex,
+//         type: BottomNavigationBarType.shifting,
+//         backgroundColor: Colors.yellow,
+//         selectedItemColor: Colors.yellow,
+//         unselectedItemColor: Colors.greenAccent,
+//         items: [
+//           BottomNavigationBarItem(
+//             backgroundColor: Colors.purple,
+//             icon: Icon(Icons.home),
+//             label: 'Home',
+//           ),
+//           BottomNavigationBarItem(
+//             backgroundColor: Colors.blue,
+//             icon: Icon(Icons.timeline),
+//             label: 'Updates',
+//           ),
+//           BottomNavigationBarItem(
+//             backgroundColor: Colors.deepOrange,
+//             icon: Icon(Icons.camera),
+//             label: 'Upload Image',
+//           ),
+//           BottomNavigationBarItem(
+//             backgroundColor: Colors.red,
+//             icon: Icon(Icons.verified_user),
+//             label: 'Community',
+//           ),
+//           BottomNavigationBarItem(
+//             backgroundColor: Colors.black,
+//             icon: Icon(Icons.call),
+//             label: 'Calls',
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
