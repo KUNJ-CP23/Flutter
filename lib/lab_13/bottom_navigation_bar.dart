b9fee772205a53e0c50959d61f94fe64521afa6f
Kunj Ramoliya <kunjramoliya367@gmail.com>
2025-01-29 11:23:03 +0530

    bottom_nav lab done

--- lib/lab_13/bottom_navigation_bar.dart
+++ lib/lab_13/bottom_navigation_bar.dart
@@ -13,65 +13,35 @@ class _BottomNavigationBar2State extends State<BottomNavigationBar2> {
   int curIndex = 0;
   List<Widget> items = [
     Container(
-        child: Text("Home"),
-        )
+      color: Colors.pinkAccent.shade100,
+        child: Center(child: Text("Home Page",style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),),)
+    ),
+    Container(
+        color: Colors.greenAccent,
+        child: Center(child: Text("About Page",style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),)
+    ),
+    Container(
+        color: Colors.blueGrey,
+        child: Center(child: Text("Person Page",style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold)),)
+    ),
   ];
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: curIndex,
+          onTap: (value){
+            setState(() {
+              curIndex = value;
+            });
+          },
+          items: [
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.home),label: 'home'),
+            BottomNavigationBarItem(icon: Icon(Icons.account_box_outlined),label: 'about'),
+            BottomNavigationBarItem(icon: Icon(CupertinoIcons.person),label: 'person'),
+          ]
+      ),
+      body: items[curIndex],
+    );
   }
 }
-
-
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-//
-// class BottomNavigationBar2 extends StatefulWidget {
-//   BottomNavigationBar2({super.key});
-//
-//
-//   @override
-//   State<BottomNavigationBar2> createState() => _BottomNavigationBar2State();
-// }
-//
-// class _BottomNavigationBar2State extends State<BottomNavigationBar2> {
-//
-//   int currentIndex=0;
-//   List<Widget> items=[
-//     Container(
-//       height: 300,
-//       width: 300,
-//       child: Center(child: Text('Home')),
-//     ),
-//     Container(
-//       height: 300,
-//       width: 300,
-//       child: Center(child: Text('About')),
-//     ),
-//     Container(
-//       height: 300,
-//       width: 300,
-//       child: Center(child: Text('Person')),
-//     ),
-//   ];
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return Scaffold(
-//       bottomNavigationBar: BottomNavigationBar(
-//           currentIndex: currentIndex,
-//           onTap: (value){
-//             setState(() {
-//               currentIndex=value;
-//             });
-//           },
-//           items: [
-//             BottomNavigationBarItem(icon: Icon(CupertinoIcons.home),label: 'home'),
-//             BottomNavigationBarItem(icon: Icon(Icons.account_box_outlined),label: 'about'),
-//             BottomNavigationBarItem(icon: Icon(CupertinoIcons.person),label: 'person'),
-//           ]),
-//
-//       body: items[currentIndex],
-//     );
-//   }
-// }
--- lib/lab_4/demo.dart
+++ lib/lab_4/demo.dart
@@ -2,14 +2,15 @@ import 'dart:io';
 void named({int? a,int? b}){
   print('named parameter : a:$a & b:$b');
 }
+//ordered optional
 void positional(int a, int b){
   print("Positional parameter : a:$a & b:$b");
 }
-void def([int a = 3,int b = 2]){
+void defa([int a = 3,int b = 2]){
 print("default parameter : a:$a & b:$b");
 }
 void main(){
   named(a:6, b:9);
   positional(3, 9);
-  def();
+  defa();
 }
