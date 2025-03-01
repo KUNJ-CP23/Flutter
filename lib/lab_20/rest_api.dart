07551cc60c938479f2bb44d53b71acb55f16d194
Kunj Ramoliya <kunjramoliya367@gmail.com>
2025-03-01 23:07:01 +0530

    rest api edit, delete

--- lib/lab_20/rest_api.dart
+++ lib/lab_20/rest_api.dart
@@ -13,17 +13,36 @@ class ApiService{
     return data;
   }
 
+  //add method --> post
   Future<void> addTodo(String title, String desc) async {
     Map<String, dynamic> maptodo = {
       "title" : title,
       "desc" : desc
     };
 
-    var res=await http.post(
+    var res = await http.post(
         Uri.parse(baseUrl),
         headers: {'Content-Type': 'application/json'},
         body: json.encode(maptodo)
     );
+  }
+
+  //edit method --> put
+  Future<void> editTodo(String id, String title, String desc) async{
+    Map<String, dynamic> updatedTodo = {
+      "title": title,
+      "desc": desc,
+    };
+    var res = await http.put(
+      Uri.parse('$baseUrl/$id'),
+      headers: {'Content-Type': 'application/json'},
+      body: json.encode(updatedTodo),
+    );
+  }
+
+  //delete method -->  delete
+  Future<void> deleteTodo (String id) async{
+    await http.delete(Uri.parse('$baseUrl/$id'));
   }
 }
 
--- lib/lab_20/todo_api.dart
+++ lib/lab_20/todo_api.dart
@@ -1,4 +1,120 @@
-import 'package:flutter/cupertino.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:labprogram/lab_20/rest_api.dart';
+//
+// class TodoApi extends StatefulWidget {
+//   const TodoApi({super.key});
+//
+//   @override
+//   State<TodoApi> createState() => _TodoApiState();
+// }
+//
+// class _TodoApiState extends State<TodoApi> {
+//   ApiService apiTodo = ApiService();
+//   List<dynamic> _tasks = [];
+//
+//   @override
+//   void initState() {
+//     super.initState();
+//     _refreshTasks();
+//   }
+//
+//   //refresh the task list
+//   void _refreshTasks() async {
+//     List tasks = await apiTodo.getAllToDo();
+//     setState(() {
+//       _tasks = tasks;
+//     });
+//   }
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('TODO WITH API'),
+//       ),
+//       body: FutureBuilder(
+//         future: apiTodo.getAllToDo(),
+//         builder: (context, snapshot){
+//           if(snapshot.connectionState==ConnectionState.waiting){
+//             return CircularProgressIndicator();
+//           }
+//           else if(snapshot.hasData){
+//             List<dynamic> data=snapshot.data!;
+//             return ListView.builder(
+//               itemCount: data.length,
+//               itemBuilder: (context, index) {
+//                 return ListTile(
+//                   title: Text(data[index]['title']),
+//                   subtitle: Text(data[index]['desc']),
+//                 );
+//               },
+//             );
+//           }
+//           else{
+//             print('NO TODO FOUND');
+//           }
+//           return CircularProgressIndicator();
+//         },
+//       ),
+//       floatingActionButton: FloatingActionButton(
+//         onPressed: () {
+//           _showAddTaskDialog(context);
+//         },
+//         child: Icon(Icons.add),
+//       ),
+//     );
+//   }
+//
+//   void _showAddTaskDialog(BuildContext context) {
+//     final titleController = TextEditingController();
+//     final descController = TextEditingController();
+//     showDialog(
+//       context: context,
+//       builder: (context) {
+//         return AlertDialog(
+//           title: Text('Add New Task'),
+//           content: Column(
+//             mainAxisSize: MainAxisSize.min,
+//             children: [
+//               TextField(
+//                 controller: titleController,
+//                 decoration: InputDecoration(labelText: 'Title'),
+//               ),
+//               TextField(
+//                 controller: descController,
+//                 decoration: InputDecoration(labelText: 'Description'),
+//               ),
+//             ],
+//           ),
+//           actions: [
+//             TextButton(
+//               onPressed: () {
+//                 Navigator.pop(context);
+//               },
+//               child: Text('Cancel'),
+//             ),
+//             ElevatedButton(
+//               onPressed: () async {
+//                 if (titleController.text.isNotEmpty) {
+//                   await apiTodo.addTodo(
+//                     titleController.text,
+//                     descController.text,
+//                   );
+//                   _refreshTasks();
+//                   Navigator.pop(context);
+//                 }
+//               },
+//               child: Text('Add'),
+//             ),
+//           ],
+//         );
+//       },
+//     );
+//   }
+// }
+
+
 import 'package:flutter/material.dart';
 import 'package:labprogram/lab_20/rest_api.dart';
 
@@ -16,9 +132,10 @@ class _TodoApiState extends State<TodoApi> {
   @override
   void initState() {
     super.initState();
+    _refreshTasks();
   }
 
-  //refresh the task list
+  // Refresh the task list
   void _refreshTasks() async {
     List tasks = await apiTodo.getAllToDo();
     setState(() {
@@ -29,45 +146,48 @@ class _TodoApiState extends State<TodoApi> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('TODO WITH API'),
-      ),
-      body: FutureBuilder(
-        future: apiTodo.getAllToDo(),
-        builder: (context, snapshot){
-          if(snapshot.connectionState==ConnectionState.waiting){
-            return CircularProgressIndicator();
-          }
-          else if(snapshot.hasData){
-            List<dynamic> data=snapshot.data!;
-            return ListView.builder(
-              itemCount: data.length,
-              itemBuilder: (context, index) {
-                return ListTile(
-                  title: Text(data[index]['title']),
-                  subtitle: Text(data[index]['desc']),
-                );
-              },
-            );
-          }
-          else{
-            print('NO TODO FOUND');
-          }
-          return CircularProgressIndicator();
+      appBar: AppBar(title: Text('TODO WITH API')),
+      body: _tasks.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+        itemCount: _tasks.length,
+        itemBuilder: (context, index) {
+          return ListTile(
+            title: Text(_tasks[index]['title']),
+            subtitle: Text(_tasks[index]['desc']),
+            trailing: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                IconButton(
+                  icon: Icon(Icons.edit, color: Colors.blue),
+                  onPressed: () => _showEditTaskDialog(
+                    context,
+                    _tasks[index]['id'],
+                    _tasks[index]['title'],
+                    _tasks[index]['desc'],
+                  ),
+                ),
+                IconButton(
+                  icon: Icon(Icons.delete, color: Colors.red),
+                  onPressed: () => _deleteTask(_tasks[index]['id']),
+                ),
+              ],
+            ),
+          );
         },
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {
-          _showAddTaskDialog(context);
-        },
+        onPressed: () => _showAddTaskDialog(context),
         child: Icon(Icons.add),
       ),
     );
   }
 
+  // Show Add Task Dialog
   void _showAddTaskDialog(BuildContext context) {
     final titleController = TextEditingController();
     final descController = TextEditingController();
+
     showDialog(
       context: context,
       builder: (context) {
@@ -76,30 +196,16 @@ class _TodoApiState extends State<TodoApi> {
           content: Column(
             mainAxisSize: MainAxisSize.min,
             children: [
-              TextField(
-                controller: titleController,
-                decoration: InputDecoration(labelText: 'Title'),
-              ),
-              TextField(
-                controller: descController,
-                decoration: InputDecoration(labelText: 'Description'),
-              ),
+              TextField(controller: titleController, decoration: InputDecoration(labelText: 'Title')),
+              TextField(controller: descController, decoration: InputDecoration(labelText: 'Description')),
             ],
           ),
           actions: [
-            TextButton(
-              onPressed: () {
-                Navigator.pop(context);
-              },
-              child: Text('Cancel'),
-            ),
+            TextButton(onPressed: () => Navigator.pop(context), child: Text('Cancel')),
             ElevatedButton(
               onPressed: () async {
                 if (titleController.text.isNotEmpty) {
-                  await apiTodo.addTodo(
-                    titleController.text,
-                    descController.text,
-                  );
+                  await apiTodo.addTodo(titleController.text, descController.text);
                   _refreshTasks();
                   Navigator.pop(context);
                 }
@@ -111,8 +217,45 @@ class _TodoApiState extends State<TodoApi> {
       },
     );
   }
+
+  // Show Edit Task Dialog
+  void _showEditTaskDialog(BuildContext context, String id, String title, String desc) {
+    final titleController = TextEditingController(text: title);
+    final descController = TextEditingController(text: desc);
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text('Edit Task'),
+          content: Column(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              TextField(controller: titleController, decoration: InputDecoration(labelText: 'Title')),
+              TextField(controller: descController, decoration: InputDecoration(labelText: 'Description')),
+            ],
+          ),
+          actions: [
+            TextButton(onPressed: () => Navigator.pop(context), child: Text('Cancel')),
+            ElevatedButton(
+              onPressed: () async {
+                if (titleController.text.isNotEmpty) {
+                  await apiTodo.editTodo(id, titleController.text, descController.text);
+                  _refreshTasks();
+                  Navigator.pop(context);
+                }
+              },
+              child: Text('Save'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  // Delete Task
+  void _deleteTask(String id) async {
+    await apiTodo.deleteTodo(id);
+    _refreshTasks();
+  }
 }
-
-
-
-
